9fb7838b4b2ccf63b0260c55b65589f2e023a9aa
Kumar <kumarofficial97@gmail.com>
2025-12-02 15:06:00 +0530

    updating provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,3 +2,15 @@ provider "aws" {
     region = "us-east-1"
     alias = "provider1"
 }
+
+
+// Another way to define the providers
+/*terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}*/
